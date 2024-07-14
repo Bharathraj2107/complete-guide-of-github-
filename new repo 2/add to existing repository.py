513905e5bba3da62db908906317d60be989460b7
Bharathraj2107 <bharathraj2105@gmail.com>
2024-07-14 13:19:09 +0530

    repo guide 2

---
+++ new repo 2/add to existing repository.py
@@ -0,0 +1,8 @@
+print("""
+To add to the existing repository
+git add .
+git commit -m "any message"
+git push -u origin master
+after changing the code in the file must be added to the repository in the same way as the above commands it will just update
+the contents in the exsiting file no new file will be created  
+""")
---
+++ new repo 2/remove the  remote and more.py
@@ -0,0 +1,6 @@
+print("""
+To remove the existing  remote origin use
+git remote remove origin
+then we can add new origin  using git remote add origin in the repository it is given
+To check the remote origin added are not use git remote -v
+""")
