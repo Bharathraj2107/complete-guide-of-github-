1c159d9e56cf973481cc8938093c40f75fafeb75
Bharathraj2107 <bharathraj2105@gmail.com>
2024-07-14 12:55:52 +0530

    new repo 1 guide

---
+++ for master repository.py
@@ -0,0 +1,13 @@
+print("""
+#after creating new repository in the github 
+#go to the folder that u want to push and do the left click go toshow more options and click on  open gitbash here
+#type the below commands in the git
+1)git init 
+2)git add .
+3)git commit -m "any message" #when we have made new changes or adding something we can use this message-it is like saving
+4)git remote add origin The origin will be given in the github when we create new repository -- it should done only once when the new repository is created
+5)git push -u origin master= it is used to push the required file into the repo
+5)when we want to create branches the use this command= git branch -M name of the branch
+6)for only one repository we use master
+7)if we have sub divisions in the main repository The main repository is called master and others are called branches
+""")
---
+++ main.py
@@ -0,0 +1,8 @@
+print("""
+#The above things should be done only onetime to make the connection betweeen the git and the github
+1)git --version #This is used to check the version of the git
+2)git config --list --show-origin
+3)git config --global user.name #it is used to check whether the git has linked to github 
+4)git config --global user.email #it is used to check whether the git has linked to github 
+5)both 3 and 4 should be  present in the (2) after doing these steps
+""")
