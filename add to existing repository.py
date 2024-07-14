13b415efbbd9afe5ad4a73453b8d819af9bdca3a
Bharathraj2107 <bharathraj2105@gmail.com>
2024-07-14 13:35:41 +0530

    new update

--- add to existing repository.py
+++ add to existing repository.py
@@ -5,4 +5,8 @@ git commit -m "any message"
 git push -u origin master
 after changing the code in the file must be added to the repository in the same way as the above commands it will just update
 the contents in the exsiting file no new file will be created  
+when we want to add new new folder or files to the existing directory then create it in the .git folder present in the already created repository where 
+we added first file 
+**when we create new folder which does not contain .git folder it gives some fatal error saying no such repository
+for new location folder we have to create a new repository only*** see and ask this once
 """)
